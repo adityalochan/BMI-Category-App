d6a55014a52025cb9a703a261e6861947dbd33b4
adityalochan <aditya.lochan25@gmail.com>
2026-09-16 17:58:10 -0400

    Code fixes

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -2,8 +2,6 @@ import sys
 
 print('Welcome to BMI calculator app')
 
-# BMI Formula: weight/height**2
-
 height = float(input("Enter your height in meters | Range 0-2.72 -- "))
 weight = float(input("Enter your weight | Range 1-700 -- "))
 
@@ -15,12 +13,12 @@ if weight<1 or weight>700 or weight is None:
     print("Invalid Entry. Exiting Program")
     sys.exit(0)
 
-bmi = height/weight**2
+bmi = weight/height**2
+
 # bmi < 18.5 : Under Weight
 # bmi < 25 : Normal Weight
 # bmi < 30 : Over Weight
 # bmi >30  : Obese
-
 condition = 'Unknown'
 
 if bmi<18.5:
@@ -32,4 +30,4 @@ elif bmi < 30:
 else:
     condition = 'Obese'
 
-print("Your BMI is {}. You are {}".format(bmi,condition))
+print(f"Your BMI is {bmi}. You are {condition}")
