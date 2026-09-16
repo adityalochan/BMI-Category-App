dd9c2f330df19762c36a838614620f7515c98150
adityalochan <aditya.lochan25@gmail.com>
2026-09-16 17:03:16 -0400

    added condition validation to code

--- bmi_calculator.py
+++ bmi_calculator.py
@@ -5,6 +5,24 @@ print('Welcome to BMI calculator app')
 height = float(input("Enter your height in meters | Range 0-2.72 -- "))
 weight = float(input("Enter your weight | Range 1-700 -- "))
 
+
+
 bmi = height/weight**2
 
-print("Your BMI is {}".format(bmi))
+# bmi < 18.5 : Under Weight
+# bmi < 25 : Normal Weight
+# bmi < 30 : Over Weight
+# bmi >30  : Obese
+
+condition = 'Unknown'
+
+if bmi<18.5:
+    condition = 'Under Weight'
+elif bmi < 25:
+    condition = 'Normal Weight'
+elif bmi < 30:
+    condition = 'Over Weight'
+else:
+    condition = 'Obese'
+
+print("Your BMI is {}. You are {}".format(bmi,condition))
